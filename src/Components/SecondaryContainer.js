ac97dfb64e432e89da34043e4b6e21fc0c998f8d
bhagyashree9025 <bhagyashree9025@gmail.com>
2025-01-02 17:20:12 +0530

    Secondary container with movie list

--- src/Components/SecondaryContainer.js
+++ src/Components/SecondaryContainer.js
@@ -1,7 +1,23 @@
 import React from "react";
+import MovieList from "./MovieList";
+import { useSelector } from "react-redux";
 
 const SecondaryContainer = () => {
-  return <div>SecondaryContainer</div>;
+  const movies = useSelector((store) => store.movie);
+  return (
+    movies?.nowPlayingMovies && (
+      <div className="bg-black">
+        <div className="-mt-40 pl-12 relative z-20">
+          <MovieList title={"Now Playing"} movies={movies?.nowPlayingMovies} />
+          <MovieList title={"Popular"} movies={movies?.popularMovies} />
+          <MovieList title={"Trending"} movies={movies?.nowPlayingMovies} />
+
+          <MovieList title={"Horror"} movies={movies?.nowPlayingMovies} />
+          <MovieList title={"Thriller"} movies={movies?.nowPlayingMovies} />
+        </div>
+      </div>
+    )
+  );
 };
 
 export default SecondaryContainer;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -15,3 +15,5 @@ export const API_OPTIONS = {
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNmY2M2Q5MTk5NDVhYTI2ZTI3ZmQ2ZGNhMjA1NWE0ZCIsIm5iZiI6MTczNTc5MDc3NS4yNCwic3ViIjoiNjc3NjEwYjYxODU0ODY1MTVkMTJhNmFjIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.gen0tvOmXK6W_82Ocs2kCYJ7fasOMdFE4nq2outHh-k",
   },
 };
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500/";
